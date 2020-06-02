f5747cf7c078c82315bfd980226d1401eeb8f0c4
Somayeh Yarahmadi <somayeh.yarahmadi@gmail.com>
2020-06-02 10:29:21 -0400

    feet: Add load_data function
    Loads data for the specified city and filters by month and day
    if applicable.

--- bikeshare.py
+++ bikeshare.py
@@ -43,3 +43,32 @@ def get_filters():
         day='all'
       return city, month, day
       #---------------------------------
+
+      def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
+    df = pd.read_csv(CITY_DATA[city])
+    # load data file into a dataframe
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+    df['month'] = df['Start Time'].dt.month
+    df['day_of_week'] = df['Start Time'].dt.day
+    if month!= 'all':
+        months = ['january', 'february', 'march', 'april', 'may', 'june']
+        month = months.index(month) + 1
+
+        # filter by month to create the new dataframe
+        df = df[df['month'] == month]
+
+    # filter by day of week if applicable
+    if day != 'all':
+        # filter by day of week to create the new dataframe
+        df = df[df['day_of_week'] == day]
+    return df
